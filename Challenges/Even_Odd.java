7f9c083898be594f41f7e7117aea2dc9eb0c14eb
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-21 23:41:34 +0530

    3  added

---
+++ Challenges/Even_Odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        float n = s.nextInt();          
+        if(n%2==0){
+            System.out.print("Even number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+    }
+}
