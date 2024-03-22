1fa0930003f6ad490e232fcd1cef1dba1bfb266d
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-22 19:36:12 +0530

    changed

--- Challenges/Ages_Separation.java
+++ Challenges/Ages_Separation.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public interface Ages_Separation {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter your age : ");
+        System.out.print("Enter your age : ");
         int age = s.nextInt();
         if(age<13){
             System.out.println("Child");
         }
-        else if(age>=13 && age<20){
+        else if(age<20){
             System.out.println("Teen");
         }
-        else if(age<60 && age>=20){
-            System.out.println("'Adult");
+        else if(age<60){
+            System.out.println("Adult");
         }
         else{
             System.out.println("Senior");
--- Challenges/Grades_Separation.java
+++ Challenges/Grades_Separation.java
@@ -6,16 +6,16 @@ public class Grades_Separation {
         System.out.print("Enter your Percentage : ");
         float per = s.nextFloat();
 
-        if(per>90.0){
+        if(per>=90.0){
             System.out.println("A Grade");
         }
         else if (per>=75.0){
             System.out.println("B Grade");
         }
-        else if (per>=60.0 && per<75.0){
+        else if (per>=60.0){
             System.out.println("C Grade");
         }
-        else if (per>=30.0 && per<60.0){
+        else if (per>=30.0){
             System.out.println("D Grade");
         }
         else{
