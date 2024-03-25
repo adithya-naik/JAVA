45219db6b80f6455f8fd8016ac3d7cf293c30fee
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-25 15:25:37 +0530

    Part 1 Completed

--- Challenges/_039_c_Pattern3Loops.java
+++ Challenges/_039_c_Pattern3Loops.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 //left half pyramid
 public class _039_c_Pattern3Loops {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of rows :");
+        int rows = s.nextInt();
+        for(int i=0;i<=rows;i++){
+            for(int j=0;j<rows-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=0;k<i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        s.close();
     }
 }
